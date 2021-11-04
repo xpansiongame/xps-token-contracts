448aaafb2ea9f75b6a96891a20a74e9d0f49f499
Nguyen Dai Duong <duongnd@Nguyens-MacBook-Pro-2.local>
2021-11-04 14:05:23 +0700

    Fix issue and update sol version

--- bsc/contracts/XpsToken.sol
+++ bsc/contracts/XpsToken.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity 0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
-contract XpsToken is ERC20, ERC20Capped, ERC20Burnable {
+contract XpsToken is  ERC20Burnable {
     uint private constant HARD_CAP = 1000_000_000e18; // 1b token
 
     /**
@@ -14,14 +13,9 @@ contract XpsToken is ERC20, ERC20Capped, ERC20Burnable {
      * @dev set name, symbol and decimal of token
      * @dev mint totalSupply (cap) to address
      */
-    constructor (
-    ) public ERC20("XPS Token", "XPS") ERC20Capped(HARD_CAP) {
-        _setupDecimals(18);
+    constructor () ERC20("XPS Token", "XPS"){
         _mint(msg.sender, HARD_CAP);
     }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Capped) {
-        super._beforeTokenTransfer(from, to, amount);
-    }
+    
 
 }
