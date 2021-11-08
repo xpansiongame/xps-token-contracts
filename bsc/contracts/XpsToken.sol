9a0fec12f4d5889857e4b15275a37b2c4269a9d0
Nguyen Dai Duong <duongnd@Nguyens-MacBook-Pro-2.local>
2021-11-08 09:46:00 +0700

    revert code

--- bsc/contracts/XpsToken.sol
+++ bsc/contracts/XpsToken.sol
@@ -2,9 +2,8 @@
 pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract XpsToken is  ERC20Burnable, Ownable {
+contract XpsToken is  ERC20Burnable {
     uint private constant HARD_CAP = 1000_000_000e18; // 1b token
 
     /**
@@ -16,10 +15,4 @@ contract XpsToken is  ERC20Burnable, Ownable {
         _mint(msg.sender, HARD_CAP);
     }
     
-    function mint(uint256 amount) external onlyOwner() {
-        require(ERC20.totalSupply() + amount <= HARD_CAP, "XpsToken: cap exceeded");
-        _mint(msg.sender, amount);
-    }
-    
-
 }
